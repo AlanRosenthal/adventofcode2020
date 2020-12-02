d09c583d699e8867fd6f0d9dd70476e11b4a428e
Alan Rosenthal <1288897+AlanRosenthal@users.noreply.github.com>
2020-12-01 21:29:04 -0500

    Changed get_list_of_permutations to accept num_groups as an arg, instead of being hard coded to 2

--- day1/report_repair.py
+++ day1/report_repair.py
@@ -5,14 +5,26 @@ Implementation of https://adventofcode.com/2020/day/1
 import click
 
 
-def get_list_of_permutations(data):
+def permutation_helper(group, data, num_groups):
     """
-    Given a list, return a generator of permutations of pairs.
-    Note: (a,b)==(b,a) and won't be counted twice
+    Recursive helper function for get_list_of_permutations
     """
-    for index, item1 in enumerate(data, start=1):
-        for item2 in data[index:]:
-            yield (item1, item2)
+    if num_groups == 0:
+        yield group
+
+    for index, item in enumerate(data, start=1):
+        new_group = list(group)
+        new_group.append(item)
+        yield from permutation_helper(new_group, data[index:], num_groups - 1)
+
+
+def get_list_of_permutations(data, num_groups):
+    """
+    Given a list, return a list of permutations of pairs of num_groups size.
+    Note: [a,b]==[b,a] and won't be counted twice
+    """
+    result = permutation_helper([], data, num_groups)
+    return list(result)
 
 
 def find_tuple_sum(data, match):
@@ -38,14 +50,14 @@ def prod(data):
     return result
 
 
-def process(input_file):
+def process(input_file, num_groups):
     """
     Read from an input file and return the answer
     """
     with open(input_file, "r") as my_file:
         data = [int(x) for x in my_file.read().splitlines()]
 
-    permutations = list(get_list_of_permutations(data))
+    permutations = get_list_of_permutations(data, num_groups)
 
     match = find_tuple_sum(permutations, 2020)
 
@@ -54,11 +66,12 @@ def process(input_file):
 
 @click.command()
 @click.option("--input_file", help="Path to input file", required=True)
-def cli(input_file=None):
+@click.option("--num_groups", help="Number of groups", required=True, type=int)
+def cli(input_file=None, num_groups=None):
     """
     CLI entry point
     """
-    result = process(input_file)
+    result = process(input_file, num_groups)
     print(result)
 
 
--- day1/test/test_report_repair.py
+++ day1/test/test_report_repair.py
@@ -12,24 +12,48 @@ def test_get_list_of_permutations_empty_list():
     """
     Test of get_list_of_permutations() with an empty list
     """
-    result = get_list_of_permutations([])
-    assert not list(result)
+    result = get_list_of_permutations([], 2)
+    assert not result
 
 
 def test_get_list_of_permutations_basic_string():
     """
     Test of get_list_of_permutations() with strings
     """
-    result = get_list_of_permutations(["a", "b", "c"])
-    assert list(result) == [("a", "b"), ("a", "c"), ("b", "c")]
+    result = get_list_of_permutations(["a", "b", "c"], 2)
+    assert result == [["a", "b"], ["a", "c"], ["b", "c"]]
 
 
-def test_get_list_of_permutations_basic_numbers():
+def test_get_list_of_permutations_basic_numbers_group_2():
     """
-    Test of get_list_of_permutations() with numbers
+    Test of get_list_of_permutations() with numbers, num_groups=2
     """
-    result = get_list_of_permutations([1, 2, 3])
-    assert list(result) == [(1, 2), (1, 3), (2, 3)]
+    result = get_list_of_permutations([1, 2, 3], 2)
+    assert result == [[1, 2], [1, 3], [2, 3]]
+
+
+def test_get_list_of_permutations_one_entry_group_2():
+    """
+    Test of get_list_of_permutations() with numbers, num_groups=2
+    """
+    result = get_list_of_permutations([1, 2], 2)
+    assert result == [[1, 2]]
+
+
+def test_get_list_of_permutations_one_entry_group_3():
+    """
+    Test of get_list_of_permutations() with numbers, num_groups=3
+    """
+    result = get_list_of_permutations([1, 2, 3], 3)
+    assert result == [[1, 2, 3]]
+
+
+def test_get_list_of_permutations_basic_numbers_group_3():
+    """
+    Test of get_list_of_permutations() with numbers, num_groups=3
+    """
+    result = get_list_of_permutations([1, 2, 3, 4], 3)
+    assert result == [[1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4]]
 
 
 def test_find_tuple_sum_empty_list():
@@ -44,15 +68,23 @@ def test_find_tuple_sum_empty_basic():
     """
     Test of find_tuple_sum() with an basic inputs
     """
-    result = find_tuple_sum([(100, 300), (400, 500), (600, 400)], 1000)
-    assert result == (600, 400)
+    result = find_tuple_sum([[100, 300], [400, 500], [600, 400]], 1000)
+    assert result == [600, 400]
+
+
+def test_find_tuple_sum_empty_basic_triples():
+    """
+    Test of find_tuple_sum() with an basic inputs, with size of three
+    """
+    result = find_tuple_sum([[100, 300, 1], [400, 500, 1], [600, 400, 1]], 1001)
+    assert result == [600, 400, 1]
 
 
 def test_find_tuple_sum_empty_no_match():
     """
     Test of find_tuple_sum() with no match
     """
-    result = find_tuple_sum([(100, 300), (400, 500), (600, 401)], 1000)
+    result = find_tuple_sum([[100, 300], [400, 500], [600, 401]], 1000)
     assert result is None
 
 
@@ -68,7 +100,7 @@ def test_prod_basic():
     """
     Test of prod() with an basic inputs, part 1
     """
-    result = prod((1, 3))
+    result = prod([1, 3])
     assert result == 3
 
 
@@ -76,7 +108,7 @@ def test_prod_basic2():
     """
     Test of prod() with an basic inputs, part 2
     """
-    result = prod((2, 3))
+    result = prod([2, 3])
     assert result == 6
 
 
@@ -84,13 +116,21 @@ def test_prod_basic_with_0():
     """
     Test of prod() with an basic inputs, that includes a 0
     """
-    result = prod((1, 0))
+    result = prod([1, 0])
     assert result == 0
+
+
+def test_prod_triple():
+    """
+    Test of prod() with an basic inputs, part 1
+    """
+    result = prod([1, 2, 3])
+    assert result == 6
 
 
 def test_end_to_end():
     """
     Test of process() with test input file
     """
-    result = process("test/test_input.txt")
+    result = process("test/test_input.txt", 2)
     assert result == 514579
