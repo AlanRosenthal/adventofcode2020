ed41946e4eb8d477aaa141487c1b24f6d266aaa4
Alan Rosenthal <1288897+AlanRosenthal@users.noreply.github.com>
2020-12-02 19:13:07 -0500

    Implemented validator v2

--- day2/password_philosophy.py
+++ day2/password_philosophy.py
@@ -24,32 +24,63 @@ def parse_password(password_str):
     }
 
 
-def is_valid_password(password):
+def is_valid_password_v1(password):
     """
-    Checks if a password is valid
+    Checks if a password is valid, with scheme 1
     """
     letter_count = sum([x == password["letter"] for x in list(password["password"])])
     return password["low"] <= letter_count <= password["high"]
 
 
-def process(input_file):
+def xor(item1, item2):
+    """
+    Implementation of xor
+    """
+    if not item1 and not item2:
+        return False
+    if item1 and item2:
+        return False
+    return True
+
+
+def is_valid_password_v2(password):
+    """
+    Checks if a password is valid, with scheme 1
+    """
+
+    low = password["letter"] == password["password"][password["low"] - 1]
+    high = password["letter"] == password["password"][password["high"] - 1]
+
+    return xor(low, high)
+
+
+def process(input_file, validator):
     """
     Process input file and returns result
     """
     with open(input_file, "r") as my_file:
         data = [parse_password(x) for x in my_file.read().splitlines()]
 
-    valid_passwords = [is_valid_password(x) for x in data]
+    # select which validator to use
+    if validator == 1:
+        validator_fn = is_valid_password_v1
+    elif validator == 2:
+        validator_fn = is_valid_password_v2
+    else:
+        raise ValueError("Invalid validator")
+
+    valid_passwords = [validator_fn(x) for x in data]
     return sum(valid_passwords)
 
 
 @click.command()
 @click.option("--input_file", help="Path to input file", required=True)
-def cli(input_file=None):
+@click.option("--validator", help="which validator to use", required=True, type=int)
+def cli(input_file=None, validator=None):
     """
     CLI entry point
     """
-    result = process(input_file)
+    result = process(input_file, validator)
     print(result)
 
 
--- day2/test/test_password_philosophy.py
+++ day2/test/test_password_philosophy.py
@@ -4,7 +4,9 @@ Test for password_philosophy.py
 
 from day2.password_philosophy import process
 from day2.password_philosophy import parse_password
-from day2.password_philosophy import is_valid_password
+from day2.password_philosophy import is_valid_password_v1
+from day2.password_philosophy import is_valid_password_v2
+from day2.password_philosophy import xor
 
 
 def test_parse_password_empty():
@@ -23,29 +25,99 @@ def test_parse_password_basic():
     assert result == {"low": 1, "high": 10, "letter": "z", "password": "abcde"}
 
 
-def test_is_valid_password_true():
+def test_is_valid_password_v1_true():
     """
-    Test of is_valid_password() with a true example
+    Test of is_valid_password_v1() with a true example
     """
-    result = is_valid_password(
+    result = is_valid_password_v1(
         {"low": 4, "high": 5, "letter": "w", "password": "awwwawaw"}
     )
     assert result
 
 
-def test_is_valid_password_false():
+def test_is_valid_password_v1_false():
     """
-    Test of is_valid_password() with a false example
+    Test of is_valid_password_v1() with a false example
     """
-    result = is_valid_password(
+    result = is_valid_password_v1(
         {"low": 3, "high": 4, "letter": "w", "password": "awwwawaw"}
     )
     assert not result
 
 
-def test_end_to_end():
+def test_is_valid_password_v2_true():
+    """
+    Test of is_valid_password_v2() with a true example
+    """
+    result = is_valid_password_v2(
+        {"low": 1, "high": 2, "letter": "w", "password": "aw"}
+    )
+    assert result
+
+
+def test_is_valid_password_v2_false1():
+    """
+    Test of is_valid_password_v2() with a false example, take 1
+    """
+    result = is_valid_password_v2(
+        {"low": 1, "high": 2, "letter": "w", "password": "ww"}
+    )
+    assert not result
+
+
+def test_is_valid_password_v2_false2():
+    """
+    Test of is_valid_password_v2() with a false example, take 2
+    """
+    result = is_valid_password_v2(
+        {"low": 1, "high": 2, "letter": "w", "password": "aa"}
+    )
+    assert not result
+
+
+def test_end_to_end_v1():
     """
     Test of end to end with test input
     """
-    result = process("test/test_input.txt")
+    result = process("test/test_input.txt", 1)
     assert result == 2
+
+
+def test_end_to_end_v2():
+    """
+    Test of end to end with test input
+    """
+    result = process("test/test_input.txt", 2)
+    assert result == 1
+
+
+def test_xor_neither():
+    """
+    Test of xor where both are false
+    """
+    result = xor(False, False)
+    assert not result
+
+
+def test_xor_both():
+    """
+    Test of xor where both are true
+    """
+    result = xor(True, True)
+    assert not result
+
+
+def test_xor_either1():
+    """
+    Test of xor where one is True
+    """
+    result = xor(True, False)
+    assert result
+
+
+def test_xor_either3():
+    """
+    Test of xor where one is True
+    """
+    result = xor(False, True)
+    assert result
