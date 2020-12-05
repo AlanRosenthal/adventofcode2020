caa7da1bc5ee83f3870d3d98eb322a2595a03998
Alan Rosenthal <1288897+AlanRosenthal@users.noreply.github.com>
2020-12-04 22:55:01 -0500

    Added stricter validator, per part 2

--- day4/passport_processing.py
+++ day4/passport_processing.py
@@ -35,9 +35,9 @@ def process_passport_attributes(passport):
     return passport_dict
 
 
-def validate_passport(passport):
+def validate_passport_weak(passport):
     """
-    Validate whether or not a passport is valid
+    Validate whether or not a passport is valid, using the weak scheme
     """
     required_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
     for key in required_keys:
@@ -47,7 +47,109 @@ def validate_passport(passport):
     return True
 
 
-def process(input_file):
+def validate_birth_year(byr):
+    """
+    Validate birth year - four digits; at least 1920 and at most 2002.
+    """
+    if len(byr) != 4:
+        return False
+    if int(byr) < 1920:
+        return False
+    if int(byr) > 2002:
+        return False
+    return True
+
+
+def validate_issue_year(iyr):
+    """
+    Validate issue year - four digits; at least 2010 and at most 2020.
+    """
+    if len(iyr) != 4:
+        return False
+    if int(iyr) < 2010:
+        return False
+    if int(iyr) > 2020:
+        return False
+    return True
+
+
+def validate_expiration_year(eyr):
+    """
+    Validate expiration year - four digits; at least 2020 and at most 2030.
+    """
+    if len(eyr) != 4:
+        return False
+    if int(eyr) < 2020:
+        return False
+    if int(eyr) > 2030:
+        return False
+    return True
+
+
+def validate_height(hgt):
+    """
+    Validate Height - a number followed by either cm or in.
+        If cm, the number must be at least 150 and at most 193.
+        If in, the number must be at least 59 and at most 76.
+    """
+    match = re.search("([0-9]*)(cm|in)", hgt)
+    if not match:
+        return False
+
+    if match[2] == "cm":
+        hgt_cm = int(match[1])
+        return 150 <= hgt_cm <= 193
+
+    if match[2] == "in":
+        hgt_in = int(match[1])
+        return 59 <= hgt_in <= 76
+
+    return False
+
+
+def validate_hair_color(hcl):
+    """
+    Validate Height - a # followed by exactly six characters 0-9 or a-f.
+    """
+    match = re.search("#[0-9a-f]{6}", hcl)
+    return bool(match)
+
+
+def validate_eye_color(ecl):
+    """
+    Validate Eye Color - exactly one of: amb blu brn gry grn hzl oth.
+    """
+    match = re.search("(amb|blu|brn|gry|grn|hzl|oth)", ecl)
+    return bool(match)
+
+
+def validate_passport_id(pid):
+    """
+    Validate PassportID - a nine-digit number, including leading zeroes.
+    """
+    match = re.search("[0-9]{9}", pid)
+    return bool(match)
+
+
+def validate_passport_strict(passport):
+    """
+    Validate whether or not a passport is valid, using the strict scheme
+    """
+    conditions = [
+        validate_passport_weak(passport),
+        validate_birth_year(passport["byr"]),
+        validate_issue_year(passport["iyr"]),
+        validate_expiration_year(passport["eyr"]),
+        validate_height(passport["hgt"]),
+        validate_hair_color(passport["hcl"]),
+        validate_eye_color(passport["ecl"]),
+        validate_passport_id(passport["pid"]),
+    ]
+
+    return all(conditions)
+
+
+def process(input_file, strict_validation):
     """
     Process input file and returns result
     """
@@ -58,18 +160,26 @@ def process(input_file):
 
     passports = [process_passport_attributes(x) for x in list(passport_attributes)]
 
-    valid_passports = [validate_passport(x) for x in passports]
+    if strict_validation:
+        validator = validate_passport_strict
+    else:
+        validator = validate_passport_weak
+
+    valid_passports = [validator(x) for x in passports]
 
     return sum(valid_passports)
 
 
 @click.command()
 @click.option("--input_file", help="Path to input file", required=True)
-def cli(input_file=None):
+@click.option(
+    "--strict_validation", help="Path to input file", required=True, type=bool
+)
+def cli(input_file=None, strict_validation=None):
     """
     CLI entry point
     """
-    result = process(input_file)
+    result = process(input_file, strict_validation)
     print(result)
 
 
--- day4/test/test_passport_processing.py
+++ day4/test/test_passport_processing.py
@@ -4,7 +4,15 @@ Test of passport_processing.py
 
 from day4.passport_processing import process_passports_data
 from day4.passport_processing import process_passport_attributes
-from day4.passport_processing import validate_passport
+from day4.passport_processing import validate_passport_weak
+from day4.passport_processing import validate_birth_year
+from day4.passport_processing import validate_issue_year
+from day4.passport_processing import validate_expiration_year
+from day4.passport_processing import validate_height
+from day4.passport_processing import validate_hair_color
+from day4.passport_processing import validate_eye_color
+from day4.passport_processing import validate_passport_id
+from day4.passport_processing import validate_passport_strict
 from day4.passport_processing import process
 
 
@@ -74,11 +82,11 @@ def test_process_passport_attributes_basic():
     assert result == {"alan": "hello", "dana": "goodbye"}
 
 
-def test_validate_passport_false():
+def test_validate_passport_weak_false():
     """
-    Test of validate_passport() where the result is False
+    Test of validate_passport_weak() where the result is False
     """
-    result = validate_passport(
+    result = validate_passport_weak(
         {
             "iyr": "2013",
             "ecl": "amb",
@@ -92,11 +100,11 @@ def test_validate_passport_false():
     assert not result
 
 
-def test_validate_passport_true():
+def test_validate_passport_weak_true():
     """
-    Test of validate_passport() where the result is True
+    Test of validate_passport_weak() where the result is True
     """
-    result = validate_passport(
+    result = validate_passport_weak(
         {
             "iyr": "2013",
             "ecl": "amb",
@@ -110,9 +118,417 @@ def test_validate_passport_true():
     assert result
 
 
-def test_end_to_end():
+def test_validate_birth_year_basic_pass():
+    """
+    Basic test of validate_birth_year() where the result is True
+    """
+    result = validate_birth_year("2002")
+    assert result
+
+
+def test_validate_birth_year_basic_fail1():
+    """
+    Basic test of validate_birth_year() where the result is False, take 1
+    """
+    result = validate_birth_year("2003")
+    assert not result
+
+
+def test_validate_birth_year_basic_fail2():
+    """
+    Basic test of validate_birth_year() where the result is False, take 2
+    """
+    result = validate_birth_year("200")
+    assert not result
+
+
+def test_validate_birth_year_basic_fail3():
+    """
+    Basic test of validate_birth_year() where the result is False, take 3
+    """
+    result = validate_birth_year("1919")
+    assert not result
+
+
+def test_validate_issue_year_pass():
+    """
+    Basic test of validate_issue_year() where the result is True
+    """
+    result = validate_issue_year("2010")
+    assert result
+
+
+def test_validate_issue_year_basic_fail1():
+    """
+    Basic test of validate_issue_year() where the result is False, take 1
+    """
+    result = validate_issue_year("2003")
+    assert not result
+
+
+def test_validate_issue_year_basic_fail2():
+    """
+    Basic test of validate_issue_year() where the result is False, take 2
+    """
+    result = validate_issue_year("200")
+    assert not result
+
+
+def test_validate_issue_year_basic_fail3():
+    """
+    Basic test of validate_issue_year() where the result is False, take 3
+    """
+    result = validate_issue_year("2022")
+    assert not result
+
+
+def test_validate_expiration_year_pass():
+    """
+    Basic test of validate_expiration_year() where the result is True
+    """
+    result = validate_expiration_year("2022")
+    assert result
+
+
+def test_validate_expiration_year_basic_fail1():
+    """
+    Basic test of validate_expiration_year() where the result is False, take 1
+    """
+    assert not validate_expiration_year("2003")
+
+
+def test_validate_expiration_year_basic_fail2():
+    """
+    Basic test of validate_expiration_year() where the result is False, take 2
+    """
+    assert not validate_expiration_year("200")
+
+
+def test_validate_expiration_year_basic_fail3():
+    """
+    Basic test of validate_expiration_year() where the result is False, take 3
+    """
+    assert not validate_expiration_year("2040")
+
+
+def test_validate_height_in_pass1():
+    """
+    Test of validate_height() using inches, passing, take 1
+    """
+    assert validate_height("59in")
+
+
+def test_validate_height_in_pass2():
+    """
+    Test of validate_height() using inches, passing, take 2
+    """
+    assert validate_height("76in")
+
+
+def test_validate_height_in_fail1():
+    """
+    Test of validate_height() using inches, failing, take 1
+    """
+    assert not validate_height("58in")
+
+
+def test_validate_height_in_fail2():
+    """
+    Test of validate_height() using inches, failing, take 2
+    """
+    assert not validate_height("77in")
+
+
+def test_validate_height_cm_pass1():
+    """
+    Test of validate_height() using centimeters, passing, take 1
+    """
+    assert validate_height("150cm")
+
+
+def test_validate_height_cm_pass2():
+    """
+    Test of validate_height() using centimeters, passing, take 2
+    """
+    assert validate_height("193cm")
+
+
+def test_validate_height_cm_fail1():
+    """
+    Test of validate_height() using centimeters, failing, take 1
+    """
+    assert not validate_height("149cm")
+
+
+def test_validate_height_cm_fail2():
+    """
+    Test of validate_height() using centimeters, failing, take 2
+    """
+    assert not validate_height("194cm")
+
+
+def test_validate_height_no_unit():
+    """
+    Test of validate_height(), without units
+    """
+    assert not validate_height("194")
+
+
+def test_validate_height_empty_string():
+    """
+    Test of validate_height(), empty string
+    """
+    assert not validate_height("")
+
+
+def test_validate_height_other_units():
+    """
+    Test of validate_height(), other units
+    """
+    assert not validate_height("194mi")
+
+
+def test_validate_hair_color_basic():
+    """
+    Basic test of validate_hair_color()
+    """
+    assert validate_hair_color("#ffffff")
+
+
+def test_validate_hair_color_fail_no_pound():
+    """
+    Basic test of validate_expiration_year() where the result is False, no pound
+    """
+    assert not validate_hair_color("ffffff")
+
+
+def test_validate_hair_color_fail_empty_string():
+    """
+    Basic test of validate_expiration_year() where the result is False, empty string
+    """
+    assert not validate_hair_color("")
+
+
+def test_validate_hair_color_fail_5_digit():
+    """
+    Basic test of validate_expiration_year() where the result is False, 5 digits
+    """
+    assert not validate_hair_color("#fffff")
+
+
+def test_validate_eye_color_amb():
+    """
+    Basic test of validate_eye_color(), where the color is amb
+    """
+    assert validate_eye_color("amb")
+
+
+def test_validate_eye_color_blu():
+    """
+    Basic test of validate_eye_color(), where the color is blu
+    """
+    assert validate_eye_color("blu")
+
+
+def test_validate_eye_color_brn():
+    """
+    Basic test of validate_eye_color(), where the color is brn
+    """
+    assert validate_eye_color("brn")
+
+
+def test_validate_eye_color_gry():
+    """
+    Basic test of validate_eye_color(), where the color is gry
+    """
+    assert validate_eye_color("gry")
+
+
+def test_validate_eye_color_grn():
+    """
+    Basic test of validate_eye_color(), where the color is grn
+    """
+    assert validate_eye_color("grn")
+
+
+def test_validate_eye_color_hzl():
+    """
+    Basic test of validate_eye_color(), where the color is hzl
+    """
+    assert validate_eye_color("hzl")
+
+
+def test_validate_eye_color_oth():
+    """
+    Basic test of validate_eye_color(), where the color is oth
+    """
+    assert validate_eye_color("oth")
+
+
+def test_validate_eye_color_fail():
+    """
+    Basic test of validate_eye_color(), where the result is False
+    """
+    assert not validate_eye_color("alan")
+
+
+def test_validate_passport_id_pass():
+    """
+    Basic test of validate_passport_id(), where the result is True
+    """
+    assert validate_passport_id("012345678")
+
+
+def test_validate_passport_id_false():
+    """
+    Basic test of validate_passport_id(), where the result is False
+    """
+    assert validate_passport_id("0123456789")
+
+
+def test_validate_passport_strict_invalid1():
+    """
+    Basic test of validate_passport_strict(), with invalid inputs, take 1
+    """
+    assert not validate_passport_strict(
+        {
+            "eyr": "1972",
+            "cid": "100",
+            "hcl": "#18171d",
+            "ecl": "amb",
+            "hgt": "170",
+            "pid": "186cm",
+            "iyr": "2018",
+            "byr": "1926",
+        }
+    )
+
+
+def test_validate_passport_strict_invalid2():
+    """
+    Basic test of validate_passport_strict(), with invalid inputs, take 2
+    """
+    assert not validate_passport_strict(
+        {
+            "iyr": "2019",
+            "hcl": "#602927",
+            "eyr": "1967",
+            "hgt": "170cm",
+            "ecl": "grn",
+            "pid": "012533040",
+            "byr": "1946",
+        }
+    )
+
+
+def test_validate_passport_strict_invalid3():
+    """
+    Basic test of validate_passport_strict(), with invalid inputs, take 3
+    """
+    assert not validate_passport_strict(
+        {
+            "hcl": "dab227",
+            "iyr": "2012",
+            "ecl": "brn",
+            "hgt": "182cm",
+            "pid": "021572410",
+            "eyr": "2020",
+            "byr": "1992",
+            "cid": "277",
+        }
+    )
+
+
+def test_validate_passport_strict_invalid4():
+    """
+    Basic test of validate_passport_strict(), with invalid inputs, take 4
+    """
+    assert not validate_passport_strict(
+        {
+            "hgt": "59cm",
+            "ecl": "zzz",
+            "eyr": "2038",
+            "hcl": "74454a",
+            "iyr": "2023",
+            "pid": "3556412378",
+            "byr": "2007",
+        }
+    )
+
+
+def test_validate_passport_strict_valid1():
+    """
+    Basic test of validate_passport_strict(), with valid inputs, take 1
+    """
+    assert validate_passport_strict(
+        {
+            "pid": "087499704",
+            "hgt": "74in",
+            "ecl": "grn",
+            "iyr": "2012",
+            "eyr": "2030",
+            "byr": "1980",
+            "hcl": "#623a2f",
+        }
+    )
+
+
+def test_validate_passport_strict_valid2():
+    """
+    Basic test of validate_passport_strict(), with valid inputs, take 2
+    """
+    assert validate_passport_strict(
+        {
+            "eyr": "2029",
+            "ecl": "blu",
+            "cid": "129",
+            "byr": "1989",
+            "iyr": "2014",
+            "pid": "896056539",
+            "hcl": "#a97842",
+            "hgt": "165cm",
+        }
+    )
+
+
+def test_validate_passport_strict_valid3():
+    """
+    Basic test of validate_passport_strict(), with valid inputs, take 3
+    """
+    assert validate_passport_strict(
+        {
+            "hcl": "#888785",
+            "hgt": "164cm",
+            "byr": "2001",
+            "iyr": "2015",
+            "cid": "88",
+            "pid": "545766238",
+            "ecl": "hzl",
+            "eyr": "2022",
+        }
+    )
+
+
+def test_validate_passport_strict_valid4():
+    """
+    Basic test of validate_passport_strict(), with valid inputs, take 4
+    """
+    assert validate_passport_strict(
+        {
+            "iyr": "2010",
+            "hgt": "158cm",
+            "hcl": "#b6652a",
+            "ecl": "blu",
+            "byr": "1944",
+            "eyr": "2021",
+            "pid": "093154719",
+        }
+    )
+
+
+def test_end_to_end_weak():
     """
     End to end test with sample input
     """
-    result = process("test/test_input.txt")
+    result = process("test/test_input.txt", False)
     assert result == 2
