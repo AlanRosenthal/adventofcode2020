e5143d527e3fd808dd1e2d36653de618fa887cab
Alan Rosenthal <1288897+AlanRosenthal@users.noreply.github.com>
2020-12-06 15:03:43 -0500

    Updated day6 for part 2

--- day6/custom_customs.py
+++ day6/custom_customs.py
@@ -3,35 +3,57 @@ Implementation of https://adventofcode.com/2020/day/6
 """
 
 from functools import reduce
+from collections import Counter
+import sys
 import click
 
 
-def group_answers(responses):
+def group_answers_anyone(responses):
     """
-    Takes a list of responses and returns the set of the group's answers
+    Takes a list of responses and returns a list of answers which anyone answered
     """
     return reduce(lambda x, y: x | y, [set(x) for x in responses])
 
 
-def process(input_file):
+def group_answers_everyone(responses):
+    """
+    Takes a list of responses and returns a list of answers which everyone answered
+    """
+
+    all_responses = reduce(lambda x, y: x + y, [list(x) for x in responses])
+    counts = Counter(all_responses)
+    return [x[0] for x in counts.items() if x[1] == len(responses)]
+
+
+def process(input_file, question):
     """
     Process input file and returns result
     """
     with open(input_file, "r") as my_file:
         data = [x.split("\n") for x in my_file.read().split("\n\n")]
 
-    answers = [group_answers(x) for x in data]
+    if question == "anyone":
+        my_filter = group_answers_anyone
+    if question == "everyone":
+        my_filter = group_answers_everyone
+
+    answers = [my_filter(x) for x in data]
 
     return sum([len(x) for x in answers])
 
 
 @click.command()
 @click.option("--input_file", help="Path to input file", required=True)
-def cli(input_file=None):
+@click.option("--question", help="Anyone vs Everyone", required=True)
+def cli(input_file=None, question=None):
     """
     CLI entry point
     """
-    result = process(input_file)
+    if not question in ["anyone", "everyone"]:
+        print("Please specify only 'anyone' or 'everyone'")
+        sys.exit(1)
+
+    result = process(input_file, question)
     print(result)
 
 
--- day6/test/test_custom_customs.py
+++ day6/test/test_custom_customs.py
@@ -3,7 +3,8 @@ Test of custom_customs.py
 """
 
 import pytest
-from day6.custom_customs import group_answers
+from day6.custom_customs import group_answers_anyone
+from day6.custom_customs import group_answers_everyone
 from day6.custom_customs import process
 
 
@@ -15,15 +16,31 @@ from day6.custom_customs import process
         (["a", "b", "c"], {"a", "b", "c"}),
     ],
 )
-def test_group_answers(responses, result):
+def test_group_answers_anyone(responses, result):
     """
     Test of group answers()
     """
-    assert group_answers(responses) == result
+    assert group_answers_anyone(responses) == result
+
+
+@pytest.mark.parametrize(
+    "responses, result",
+    [
+        (["a", "ab", "abc"], ["a"]),
+        (["a"], ["a"]),
+        (["a", "b", "c"], []),
+        (["abc"], ["a", "b", "c"]),
+    ],
+)
+def test_group_answers_everyone(responses, result):
+    """
+    Test of group answers()
+    """
+    assert group_answers_everyone(responses) == result
 
 
 def test_end_to_end():
     """
     End to end test of test input file
     """
-    assert process("test/test_input.txt") == 11
+    assert process("test/test_input.txt", "anyone") == 11
